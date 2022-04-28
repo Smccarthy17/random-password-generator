59a30da30b037cf6aadc9ad2431f28628f464c43
S. McCarthy <90977936+Smccarthy17@users.noreply.github.com>
2022-04-28 12:10:37 -0400

    added function if statements to gen pwd

---
+++ script.js
@@ -0,0 +1,26 @@
+// Generator character arrays
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var specialCh = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"]
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+//Button//
+var generateBtn = document.querySelector("#generate");
+
+//Function & if statements to generate password//
+function generatePassword() {
+    var length = prompt("Enter Number of characters between 8-128.");
+    console.log (length);
+    var finalPassword = ""
+    
+    if (length < 8 || length > 128) {
+      alert("Choose a number between 8 and 128");
+      return (" ")
+    }
+  
+    if(isNaN(length)){
+      alert("Choose a number between 8 and 128 then click the generate password button.");
+      return (" ")
+    }
+
+    
